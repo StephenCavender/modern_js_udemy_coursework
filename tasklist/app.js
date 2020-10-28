8445c3a040e773dd09c34c5ba44ba344b2f0ad8c
Stephen Cavender <stephen.cavender@genesys.com>
2020-10-28 14:05:36 -0400

    delete and filter logic

--- tasklist/app.js
+++ tasklist/app.js
@@ -8,27 +8,68 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
-    // Add task event
-    form.addEventListener('submit', addTask);
+  // Add task event
+  form.addEventListener('submit', addTask);
+
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+
+  // Clear tasks event
+  clearBtn.addEventListener('click', clearTasks);
+
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
-    if (taskInput.value === '') {
-        alert('Add a task');
+  if (taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link)
+
+  taskList.appendChild(li);
+
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
     }
+  }
+}
 
-    const li = document.createElement('li');
-    li.className = 'collection-item';
-    li.appendChild(document.createTextNode(taskInput.value));
+function clearTasks() {
+  // taskList.innerHTML = '';
 
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    li.appendChild(link)
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 
-    taskList.appendChild(li);
+  // https://jsperf.com/innerhtml-vs-removechild
+}
 
-    taskInput.value = '';
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
 
-    e.preventDefault();
+  document.querySelectorAll('.collection-item').forEach(task => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
 }
